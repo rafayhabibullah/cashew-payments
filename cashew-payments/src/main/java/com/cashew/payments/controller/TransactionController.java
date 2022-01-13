ac6fbaac3a42db1160e219d82f931438a4ea00e1
rafayhabibullah <rafay_habibullah@hotmail.com>
2022-01-13 23:32:58 +0400

    add improved logging in transaction controller

--- cashew-payments/src/main/java/com/cashew/payments/controller/TransactionController.java
+++ cashew-payments/src/main/java/com/cashew/payments/controller/TransactionController.java
@@ -1,5 +1,7 @@
 package com.cashew.payments.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +18,21 @@ import com.cashew.payments.validator.RequestValidator;
 @RestController
 public class TransactionController {
 	
+	private static final Logger log = LoggerFactory.getLogger(TransactionController.class);
+	
 	@Autowired
 	TransactionService transactionService;
 	
 	@RequestMapping(value = "/transfer", method = RequestMethod.POST)
 	public ResponseEntity<Account> transfer(@RequestBody Transaction transaction) {
-		if(RequestValidator.isInvalidTransactionRequest(transaction)) 
+		log.info("transfer service - started");
+		if(RequestValidator.isInvalidTransactionRequest(transaction)) {
+			log.info("transfer service - invalid request");
 			return new ResponseEntity<Account>(HttpStatus.UNPROCESSABLE_ENTITY);
+		}
 			
 		Account account = transactionService.transfer(transaction);
+		log.info("transfer service - successfully processed");
 		return new ResponseEntity<Account>(account, HttpStatus.CREATED);
 	}
 }
