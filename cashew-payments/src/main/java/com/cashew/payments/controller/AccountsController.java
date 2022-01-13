7de1d00fdcf1b6a0d192ac2162a88862e01e315c
rafayhabibullah <rafay_habibullah@hotmail.com>
2022-01-13 19:50:58 +0400

    add improved logging in AccountsController

--- cashew-payments/src/main/java/com/cashew/payments/controller/AccountsController.java
+++ cashew-payments/src/main/java/com/cashew/payments/controller/AccountsController.java
@@ -2,6 +2,8 @@ package com.cashew.payments.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +16,24 @@ import com.cashew.payments.service.AccountsService;
 
 @RestController
 public class AccountsController {
+	
+	private static final Logger log = LoggerFactory.getLogger(AccountsController.class);
 
 	@Autowired
 	AccountsService accountsService;
 	
 	@RequestMapping(value = "/accounts", method = RequestMethod.GET)
 	public ResponseEntity<List<Account>> getAllAccounts() {
+		log.info("getAllAccounts service - started");
 		List<Account> accounts = accountsService.getAllAccounts();
-		if(accounts.isEmpty())
+		if(accounts.isEmpty()) {
+			log.info("getAllAccounts service - no content found");
 			return new ResponseEntity<List<Account>>(accounts, HttpStatus.NO_CONTENT);
-		else 
+		}
+		else {
+			log.info("getAllAccounts service - content found");
+			log.debug("getAllAccounts service : " + accounts.toString());
 			return new ResponseEntity<List<Account>>(accounts, HttpStatus.OK);
+		}
 	}
 }
