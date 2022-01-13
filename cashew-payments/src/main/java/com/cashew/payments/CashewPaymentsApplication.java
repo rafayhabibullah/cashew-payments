e65287f8f224739d1be0c35ec30a8672ae6d71b1
rafayhabibullah <rafay_habibullah@hotmail.com>
2022-01-13 15:20:07 +0400

    add project structure

---
+++ cashew-payments/src/main/java/com/cashew/payments/CashewPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.cashew.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashewPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashewPaymentsApplication.class, args);
+	}
+
+}
---
+++ cashew-payments/src/test/java/com/cashew/payments/CashewPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashew.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashewPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
